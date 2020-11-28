0baadd6a64ff2d23c2efb9913c6383607f06d6a2
ElBonn <classroom1291@gmail.com>
2020-11-28 00:05:05 -0500

    primer commir, subiendo rufuero a git

---
+++ src/Refuerzo/Refuerzo.js
@@ -0,0 +1,379 @@
+import React from "react";
+import "../Componentes/refuerzo.css";
+import lincler from "../Imagenes/Abradolf_Lincler.png";
+import beth from "../Imagenes/Beth_Smith.png";
+import bird from "../Imagenes/bird_person.png";
+import evil from "../Imagenes/Evil_Morty.png";
+import gloo from "../Imagenes/Glootie.png";
+import hammer from "../Imagenes/HammerMorty.png";
+import jerry from "../Imagenes/Jerry_Smith.png";
+import jessi from "../Imagenes/jessica.png";
+import morty from "../Imagenes/Morty_smith.png";
+import meeseeks from "../Imagenes/Mr_Meeseeks.png";
+import poopy from "../Imagenes/Mr_Poopy_Butthole.png";
+import noob from "../Imagenes/noob_noob.png";
+import pickle from "../Imagenes/Pickle_Rick.png";
+import camaron from "../Imagenes/Rick_camaron.png";
+import rick from "../Imagenes/rick_sanchez.png";
+import ruben from "../Imagenes/Ruben.png";
+import snuffles from "../Imagenes/Snuffles.png";
+import squanchy from "../Imagenes/Squanchy.png";
+import summer from "../Imagenes/Summer_Smith.jpg";
+import tiny from "../Imagenes/TinyRick.png";
+
+function Info(params) {
+  return (
+    <div className="fondo">
+        <h1 className="h11">PERSONAJES RICK AND MORTY</h1>
+      <div className="divicion">
+        <div className="con1">
+          <img className="contenedor" src={rick} />
+          <center>
+            <div>
+              <center>
+                <h3>RICK SANCHEZ</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: 70 años</p>
+              <p>Planeta de origen: Tierra (C-137)</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="contenedor" src={morty} />
+          <center>
+            <div>
+              <center>
+                <h3>MORTY SMITH</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: 14 años</p>
+              <p>Origen: Tierra (C-137)</p>
+            </div>
+          </center>
+        </div>
+      </div>
+
+      <div className="con2" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={summer} />
+          <center>
+            <div>
+              <center>
+                <h3>SUMMER SMITH</h3>
+              </center>
+              <p>Estado: Viva</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Femenino</p>
+              <p>Edad: 17 años</p>
+              <p>Origen: Tierra (Dimensión de reemplazo)</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={beth} />
+          <center>
+            <div>
+              <center>
+                <h3>BETH SMITH</h3>
+              </center>
+              <p>Estado: Viva</p>
+              <p>Especie: Humano</p>
+              <p>Edad: 34</p>
+              <p>Genero: Femenina</p>
+              <p>Origen: Tierra (Dimensión de reemplazo)</p>
+            </div>
+          </center>
+        </div>
+      </div>
+
+      <div className="con2" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="contenedor" src={gloo} />
+          <center>
+            <div>
+              <center>
+                <h3>GLOOTIE</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Monogatron</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Desconocido</p>
+              <p>No hagas su sitio web</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="contenedor" src={jerry} />
+          <center>
+            <div>
+              <center>
+                <h3>JERRY SMITH</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: 35</p>
+              <p>Origen: Tierra</p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="con2" />
+      <div className="divicion">
+        <div className="con1">
+          <img className="contenedor" src={jessi} />
+          <center>
+            <div>
+              <center>
+                <h3>JESSICA</h3>
+              </center>
+              <p>Estado: Viva</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: 15 años</p>
+              <p>Origen: Tierra </p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="contenedor" src={hammer} />
+          <center>
+            <div>
+              <center>
+                <h3>HAMMERHEAD MORTY</h3>
+              </center>
+              <p>Estado: Desconocido</p>
+              <p>Especie: Desconocido</p>
+              <p>Genero: Maculino</p>
+              <p>Edad: 14 años</p>
+              <p>Origen: Desconocio</p>
+            </div>
+          </center>
+        </div>
+      </div>
+
+      <div className="con2" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="contenedor" src={ruben} />
+          <center>
+            <div>
+              <center>
+                <h3>RUBEN</h3>
+              </center>
+              <p>Estado: Muerto</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: Desconocida</p>
+              <p>Origen: Tierra </p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="contenedor" src={tiny} />
+          <center>
+            <div>
+              <center>
+                <h3>TINY RICK</h3>
+              </center>
+              <p>Estado: Muerto</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: 14</p>
+              <p>Origen: Tierra </p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="con2" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="contenedor" src={snuffles} />
+          <center>
+            <div>
+              <center>
+                <h3>SNUFFLES</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Canina</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra (Dimensión C-137)</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="contenedor" src={lincler} />
+          <center>
+            <div>
+              <center>
+                <h3>ABRADOLF LINCLER</h3>
+              </center>
+              <p>Estado: Desconocido</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: Desconocida</p>
+              <p>Origen: Tierra36</p>
+            </div>
+          </center>
+        </div>
+      </div>
+
+      <div className="con2" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="contenedor" src={bird} />
+          <center>
+            <div>
+              <center>
+                <h3>BIRDPERSON</h3>
+              </center>
+              <p>Estado: Fallecida</p>
+              <p>Especie: Cyborg</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: Desconocida</p>
+              <p>Origen: Mundo de las aves</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="contenedor" src={evil} />
+          <center>
+            <div>
+              <center>
+                <h3>EVIL MORTY</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Cyborg</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra(dimension desconocida)</p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="con2" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="contenedor" src={meeseeks} />
+          <center>
+            <div>
+              <center>
+                <h3>MR. MEESEEKS</h3>
+              </center>
+              <p>Estado: Vivo/Muerto</p>
+              <p>Especie: Desconocida</p>
+              <p>Genero: Desconocido</p>
+              <p>Origen: Cuadro meeseek</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="contenedor" src={poopy} />
+          <center>
+            <div>
+              <center>
+                <h3>MR. POOPYBUTTHOLE</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Desconocida</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: Desconocida</p>
+              <p>Origen: Tierra </p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="con2" />
+      <div className="divicion">
+        <div className="con1">
+          <img className="contenedor" src={noob} />
+          <center>
+            <div>
+              <center>
+                <h3>NOOB NOOB</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Desconocida</p>
+              <p>Genero: Maculino</p>
+              <p>Edad: desconocida</p>
+              <p>Origen: Desconocido</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="contenedor" src={pickle} />
+          <center>
+            <div>
+              <center>
+                <h3>PICKLE RICK</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Pepinillo</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: 70 años</p>
+              <p>Origen: Gazorpazorp</p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="con2" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="contenedor" src={camaron} />
+          <center>
+            <div>
+              <center>
+                <h3>RICK CAMARON</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: desconocida </p>
+              <p>Genero: Masculino</p>
+              <p>Edad: 70 años</p>
+              <p>Origen: Tierra (dimension desconocida) </p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="contenedor" src={squanchy} />
+          <center>
+            <div>
+              <center>
+                <h3>SQUANCHY</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Squanchies</p>
+              <p>Genero: Masculino</p>
+              <p>Edad: Desconocida</p>
+              <p>Origen: Desconocido</p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="con2" />
+    </div>
+  );
+}
+export default Info;
